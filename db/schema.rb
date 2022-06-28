99182d2fdaf27051ab4e2475af227b5f06af90d2
Raghave Gupta <raghave18@gmail.com>
2022-06-28 19:52:29 +1000

    error fix

--- db/schema.rb
+++ db/schema.rb
@@ -37,10 +37,10 @@ ActiveRecord::Schema.define(version: 2022_06_25_052455) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "url"
     t.string "location"
     t.float "latitude"
     t.float "longitude"
-    t.string "url"
     t.index ["user_id"], name: "index_buddies_on_user_id"
   end
 
