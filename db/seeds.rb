041a3ff33dcbb323d845f68486a1e4df23693633
Darko Petkovic <darko.petkovic.05@gmail.com>
2022-06-30 19:23:47 +1000

    fixed seed address

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,8 @@ require 'faker'
 require "json"
 require "rest-client"
 
+street = ["Flinders St VIC 3000", "Collins St VIC 3000", "La Trobe Street VIC 3000", "Lonsdale St VIC 3000"]
+
 Booking.destroy_all
 Buddy.destroy_all
 User.destroy_all
@@ -19,7 +21,7 @@ user = User.create!(email: "beth@gmail.com", password: "password" )
 4.times do
   response = RestClient.get "https://dog.ceo/api/breeds/image/random"
   repos = JSON.parse(response)
-  buddy = Buddy.create!(name: Faker::Creature::Dog.name , buddy_type: Faker::Creature::Dog.breed , skills: Faker::Hobby.activity, description: Faker::Creature::Dog.meme_phrase , activities: Faker::Creature::Dog.sound , url: repos["message"] , location: Faker::Address.city , rate: rand(300), user_id: user.id)
+  buddy = Buddy.create!(name: Faker::Creature::Dog.name , buddy_type: Faker::Creature::Dog.breed , skills: Faker::Hobby.activity, description: Faker::Creature::Dog.meme_phrase , activities: Faker::Creature::Dog.sound , url: repos["message"] , location: "#{rand(300)} #{street.sample}" , rate: rand(300), user_id: user.id)
   Booking.create!(start_date: "28/6/2022", end_date: "28/6/2022", status: "pending", user_id: user.id, buddy_id: buddy.id)
   Booking.create!(start_date: "28/6/2022", end_date: "28/6/2022", status: "declined", user_id: user.id, buddy_id: buddy.id)
   Booking.create!(start_date: "28/6/2022", end_date: "28/6/2022", status: "Accepted", user_id: user.id, buddy_id: buddy.id)
@@ -30,7 +32,7 @@ user2 = User.create!(email: "moo@gmail.com", password: "password" )
 4.times do
   response = RestClient.get "https://dog.ceo/api/breeds/image/random"
   repos = JSON.parse(response)
-    buddy = Buddy.create!(name: Faker::Creature::Dog.name , buddy_type: Faker::Creature::Dog.breed , skills: Faker::Hobby.activity, description: Faker::Creature::Dog.meme_phrase , activities: Faker::Creature::Dog.sound , url: repos["message"] , location: Faker::Address.city, rate: rand(300), user_id: user2.id)
+    buddy = Buddy.create!(name: Faker::Creature::Dog.name , buddy_type: Faker::Creature::Dog.breed , skills: Faker::Hobby.activity, description: Faker::Creature::Dog.meme_phrase , activities: Faker::Creature::Dog.sound , url: repos["message"] , location: "#{rand(300)} #{street.sample}", rate: rand(300), user_id: user2.id)
     Booking.create!(start_date: "28/6/2022", end_date: "28/6/2022", status: "pending", user_id: user2.id, buddy_id: buddy.id)
     Booking.create!(start_date: "28/6/2022", end_date: "28/6/2022", status: "Declined", user_id: user2.id, buddy_id: buddy.id)
     Booking.create!(start_date: "28/6/2022", end_date: "28/6/2022", status: "Accepted", user_id: user2.id, buddy_id: buddy.id)
@@ -44,5 +46,5 @@ end
 20.times do
   response = RestClient.get "https://dog.ceo/api/breeds/image/random"
   repos = JSON.parse(response)
-    buddy = Buddy.create!(name: Faker::Creature::Dog.name , buddy_type: Faker::Creature::Dog.breed , skills: Faker::Hobby.activity, description: Faker::Creature::Dog.meme_phrase , activities: Faker::Creature::Dog.sound , url: repos["message"] , location: Faker::Address.full_address , rate: rand(300), user_id: user.id)
+    buddy = Buddy.create!(name: Faker::Creature::Dog.name , buddy_type: Faker::Creature::Dog.breed , skills: Faker::Hobby.activity, description: Faker::Creature::Dog.meme_phrase , activities: Faker::Creature::Dog.sound , url: repos["message"] , location:  "#{rand(300)} #{street.sample}" , rate: rand(300), user_id: user.id)
 end
